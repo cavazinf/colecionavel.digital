f2e1384c34721eaded497fc000871aeda316b7a6
samuelsm <samuelmeurer13@gmail.com>
2022-09-28 22:43:55 -0300

    Adicionando processo de deploy do contrato.

---
+++ scripts/deploy.mjs
@@ -0,0 +1,16 @@
+async function deployContract() {
+    const NFT = await ethers.getContractFactory("colecionavel.digital")
+    const nft = await NFT.deploy()
+    await nft.deployed()
+    const txHash = nft.deployTransaction.hash
+    const txReceipt = await ethers.provider.waitForTransaction(txHash)
+    const contractAddress = txReceipt.contractAddress
+    console.log("Contrato implantado no endereço:", contractAddress)
+   }
+   
+   deployContract()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
